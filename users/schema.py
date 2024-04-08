b9a98e94d12f9b8ab1da6f00cb4d750ea5d1f0d4
Alphasphinx <jerrysmauel06@gmail.com>
2024-04-08 19:17:09 +0100

    added schema for auth

--- users/schema.py
+++ users/schema.py
@@ -3,3 +3,6 @@ from graphene_django import DjangoObjectType
 from graphql import GraphQLError
 from users.models import Account as User, Profile
 
+class AuthMutation(graphene.Mutation):
+    pass
+
